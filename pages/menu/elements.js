11858c30775e7ed63582b5431a7fe3e0ad78ff58
luis13022001 <luis.filipe@ufu.br>
2026-02-12 15:50:54 -0300

    docs: change elements page menu

--- pages/menu/elements.js
+++ pages/menu/elements.js
@@ -1,5 +1,5 @@
 export const elements = {
   menu_parcelas: '[id=menu-parcelas]',
   menu_parcelas_vencidas: '[name=tab-vencidas]',
-  LoginButton: '#loginButton'
+  parcela_vencida: '[data-cy=parcela-item]'
 }
